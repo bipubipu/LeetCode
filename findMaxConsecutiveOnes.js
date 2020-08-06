131b697d27a1516a6812964e8bc5f33716301c39
bipubipu <wyqatm@gmail.com>
2020-08-06 11:58:23 +1000

    Update findMaxConsecutiveOnes.js

--- findMaxConsecutiveOnes.js
+++ findMaxConsecutiveOnes.js
@@ -10,9 +10,17 @@ var findMaxConsecutiveOnes = function(nums) {
  /**
  * This question can be summarised as finding the longest element in an array.
  * https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3238/
- * The next method is slower.
+ * The next solution is slower.
  */
  
  var findMaxConsecutiveOnes2 = function(nums) {
     return nums.join('').split("0").reduce((result, val) => Math.max(result, val.length), 0);
  }   
+
+var findMaxConsecutiveOnes3 = function(nums) {
+    let max =0, result = 0;
+    for (let i=0; i < nums.length; i++) {
+        max = Math.max(max, result = nums[i] == 0 ? 0 : result + 1);
+    }
+    return max;
+};
